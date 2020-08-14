9e21f79e9a5faa38b92ef06059ad35c85c0893e3
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-14 17:18:29 -0300

    feature: Add invalid file content error

--- errors.go
+++ errors.go
@@ -12,3 +12,13 @@ type ErrInvalidFile struct {
 func (e *ErrInvalidFile) Error() string {
 	return fmt.Sprintf("Invalid file: %s", e.file)
 }
+
+// ErrInvalidFileContent error
+type ErrInvalidFileContent struct {
+	file    string
+	content string
+}
+
+func (e *ErrInvalidFileContent) Error() string {
+	return fmt.Sprintf("Invalid file content: %s\n%s", e.file, e.content)
+}
--- errors_test.go
+++ errors_test.go
@@ -1,6 +1,7 @@
 package subtitles
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +11,13 @@ func TestErrInvalidFile(t *testing.T) {
 	err := &ErrInvalidFile{file: "invalid-file.srt"}
 
 	assert.EqualError(t, err, "Invalid file: invalid-file.srt")
+}
+
+func TestErrInvalidFileContent(t *testing.T) {
+	err := &ErrInvalidFileContent{
+		file:    "invalid-file-content",
+		content: "Invalid Content",
+	}
+
+	assert.EqualError(t, err, fmt.Sprintf("Invalid file content: %s\n%s", err.file, err.content))
 }
