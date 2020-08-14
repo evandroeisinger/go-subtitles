fd892ebf8e559dcc2de75c4d731b7f9cb42ea455
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-14 15:53:40 -0300

    fetaure: Returns an error when loading on invalid file

--- subtitles.go
+++ subtitles.go
@@ -1,9 +1,17 @@
 package subtitles
 
+import (
+	"os"
+)
+
 // Subtitle struct
 type Subtitle struct{}
 
 // LoadFile method
 func LoadFile(file string) (Subtitle, error) {
+	if _, err := os.Stat(file); err != nil {
+		return Subtitle{}, &ErrInvalidFile{file: file}
+	}
+
 	return Subtitle{}, nil
 }
--- subtitles_test.go
+++ subtitles_test.go
@@ -1,14 +1,40 @@
 package subtitles
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadFile(t *testing.T) {
-	subtitle, err := LoadFile("testdata/sample.srt")
+	fileTests := []struct {
+		path string
+	}{
+		{path: "testdata/sample.srt"},
+	}
 
-	assert.Equal(t, subtitle, Subtitle{}, "should returns a subtitle")
-	assert.Equal(t, err, nil, "not returns error")
+	for _, file := range fileTests {
+		subtitle, err := LoadFile(file.path)
+
+		assert.Equal(t, subtitle, Subtitle{}, "should returns a subtitle")
+		assert.Nil(t, err, "not returns error")
+	}
+}
+
+func TestInvalidLoadFile(t *testing.T) {
+	invalidFileTests := []struct {
+		path string
+	}{
+		{path: "testdata/invalid-path.srt"},
+		{path: "testdata/invalid-file.srt"},
+	}
+
+	for _, file := range invalidFileTests {
+		subtitle, err := LoadFile(file.path)
+
+		assert.NotNil(t, err, "should returns a Error")
+		assert.EqualError(t, err, fmt.Sprintf("Invalid file: %s", file.path))
+		assert.Equal(t, subtitle, Subtitle{}, "should returns a empty subtitle")
+	}
 }
