662fb7d9b474819e3d3b721475e7bd24ab3b4370
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-18 18:39:33 -0300

    test: Read simple SRT block

--- srt_test.go
+++ srt_test.go
@@ -13,3 +13,12 @@ func TestReadEmptySubtitle(t *testing.T) {
 	assert.Equal(t, &Subtitle{}, subtitle)
 	assert.Nil(t, err)
 }
+
+func TestReadSimpleBlock(t *testing.T) {
+	content := strings.NewReader("1\n00:00:00,000 --> 00:00:01,000\nLorem ipsum dolor sit amet\n")
+
+	subtitle, err := NewSRT().Read(content)
+
+	assert.Equal(t, 1, len(subtitle.blocks))
+	assert.Nil(t, err)
+}
