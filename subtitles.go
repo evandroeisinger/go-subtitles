8eba86e9874f4aab35f15774ff8c78d162323b4f
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-18 19:02:48 -0300

    feature: Format for file

--- subtitles.go
+++ subtitles.go
@@ -1,5 +1,17 @@
 package subtitles
 
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+)
+
+// Format interface
+type Format interface {
+	Read(reader io.Reader) (*Subtitle, error)
+	Write(writer io.Writer) error
+}
+
 // Block struct
 type Block struct {
 	lines   []string
@@ -11,6 +23,34 @@ type Block struct {
 type Subtitle struct {
 	content string
 	blocks  []Block
+}
+
+// ErrUnsupportedExtension error
+type ErrUnsupportedExtension struct {
+	extension string
+}
+
+func (e *ErrUnsupportedExtension) Error() string {
+	return fmt.Sprintf("Unsupported extension: %s", e.extension)
+}
+
+// FormatForFile returns format
+func FormatForFile(path string) (Format, error) {
+	fileExtension := filepath.Ext(path)
+
+	var format Format
+	var err error
+
+	switch fileExtension {
+	case ".srt":
+		format = NewSRT()
+	default:
+		err = &ErrUnsupportedExtension{
+			extension: fileExtension,
+		}
+	}
+
+	return format, err
 }
 
 // Load method
--- subtitles_test.go
+++ subtitles_test.go
@@ -6,6 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFormatForFileSRT(t *testing.T) {
+	format, err := FormatForFile("valid.srt")
+
+	assert.Equal(t, NewSRT(), format)
+	assert.Nil(t, err)
+}
+
+func TestFormatForInvalidFileFormat(t *testing.T) {
+	format, err := FormatForFile("invalid.mp4")
+
+	assert.Nil(t, format)
+	assert.EqualError(t, err, "Unsupported extension: .mp4")
+}
+
 func TestLoad(t *testing.T) {
 	subtitle, err := Load("testdata/empty.srt")
 
