2d2cb731703838cd789d61e62a011e351934e1fb
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-20 12:15:05 -0300

    feature: Check if file is empty func

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package subtitles
 
-import "strings"
+import (
+	"os"
+	"strings"
+)
 
 // BOMUnicode value
 const BOMUnicode = "\ufeff"
@@ -8,4 +11,15 @@ const BOMUnicode = "\ufeff"
 // StripBOM removes bytes order mark from string
 func StripBOM(s string) string {
 	return strings.Trim(s, BOMUnicode)
+}
+
+// IsEmptyFile checks if file has content
+func IsEmptyFile(p string) bool {
+	fileInfo, err := os.Stat(p)
+
+	if err != nil {
+		return true
+	}
+
+	return fileInfo.Size() == 0
 }
--- utils_test.go
+++ utils_test.go
@@ -9,3 +9,8 @@ import (
 func TestStripBom(t *testing.T) {
 	assert.Equal(t, "Lorem ipsum", StripBOM("\ufeffLorem ipsum"))
 }
+
+func TestIsEmptyFile(t *testing.T) {
+	assert.True(t, IsEmptyFile("testdata/empty.srt"))
+	assert.False(t, IsEmptyFile("testdata/sample.srt"))
+}
