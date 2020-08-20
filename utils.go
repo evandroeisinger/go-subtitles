e25175e47cf73a2499d9067ad88ff00ed74389a2
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2020-08-20 12:26:59 -0300

    feature: Validate file utils

--- utils.go
+++ utils.go
@@ -13,6 +13,17 @@ func StripBOM(s string) string {
 	return strings.Trim(s, BOMUnicode)
 }
 
+// IsFile checks if its a file
+func IsFile(p string) bool {
+	fileInfo, err := os.Stat(p)
+
+	if err != nil {
+		return false
+	}
+
+	return !fileInfo.IsDir()
+}
+
 // IsEmptyFile checks if file has content
 func IsEmptyFile(p string) bool {
 	fileInfo, err := os.Stat(p)
@@ -22,4 +33,13 @@ func IsEmptyFile(p string) bool {
 	}
 
 	return fileInfo.Size() == 0
+}
+
+// FileExist checks if its a valid file
+func FileExist(p string) bool {
+	if _, err := os.Stat(p); err != nil {
+		return false
+	}
+
+	return true
 }
--- utils_test.go
+++ utils_test.go
@@ -10,7 +10,17 @@ func TestStripBom(t *testing.T) {
 	assert.Equal(t, "Lorem ipsum", StripBOM("\ufeffLorem ipsum"))
 }
 
+func TestIsFile(t *testing.T) {
+	assert.True(t, IsFile("testdata/sample.srt"))
+	assert.False(t, IsFile("testdata/"))
+}
+
 func TestIsEmptyFile(t *testing.T) {
 	assert.True(t, IsEmptyFile("testdata/empty.srt"))
 	assert.False(t, IsEmptyFile("testdata/sample.srt"))
+}
+
+func TestFileExist(t *testing.T) {
+	assert.True(t, FileExist("testdata/sample.srt"))
+	assert.False(t, FileExist("testdata/invalid.srt"))
 }
